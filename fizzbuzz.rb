f4f1a3699808995b2f01161a342439466fa79bc2
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-26 04:40:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-   if int % (3 && 5) == 0
-    "FizzBuzz"
+   if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  elsif int % (3 && 5) == 0
+    "FizzBuzz"
   else int % (3 || 5)  == 0
     puts
   end  
